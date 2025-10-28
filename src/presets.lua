ce9be4affd26559db0e4b698bbb1ad154ef2e0c2
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-29 05:53:06 +0800

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -22,39 +22,34 @@ return {
         }
     };
     ["Weak"] = {
-        -- The default LuaVersion is Lua51
-        LuaVersion = "Lua51";
-        -- For minifying no VarNamePrefix is applied
-        VarNamePrefix = "";
-        -- Name Generator for Variables that look like this: IlI1lI1l
-        NameGenerator = "Il";
-        -- No pretty printing
-        PrettyPrint = false;
-        -- Seed is generated based on current time
-        Seed = 0;
-        -- Obfuscation steps
-        Steps = {
-            {
-                Name = "Vmify";
-                Settings = {
-                    
-                };
-            },
-            {
-                Name = "ConstantArray";
-                Settings = {
-                    Treshold    = 1;
-                    StringsOnly = true;
-                }
-            },
-            {
-                Name = "WrapInFunction";
-                Settings = {
+    LuaVersion = "Lua51";
+    VarNamePrefix = "";
+    NameGenerator = "Il";
+    PrettyPrint = false;
+    Seed = 0;
+    Steps = {
+        {
+            Name = "Vmify";
+            Settings = {
+                
+            };
+        },
+        {
+            Name = "ConstantArray";
+            Settings = {
+                Treshold    = 1;
+                StringsOnly = true;
+                Encoding = "base64"; 
+            }
+        },
+        {
+            Name = "WrapInFunction";
+            Settings = {
 
-                }
-            },
-        }
-    };
+            }
+        },
+    }
+};
     ["Medium"] = {
         -- The default LuaVersion is Lua51
         LuaVersion = "Lua51";
