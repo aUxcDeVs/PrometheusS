449167cccb4ee0dacc31ef1f359fff1f492addf9
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-29 18:05:45 +0800

    Update namegenerators.lua

--- src/prometheus/namegenerators.lua
+++ src/prometheus/namegenerators.lua
@@ -4,4 +4,5 @@ return {
 	Il = require("prometheus.namegenerators.Il");
 	Number = require("prometheus.namegenerators.number");
 	Confuse = require("prometheus.namegenerators.confuse");
+	MangledFusedNumber = require("prometheus.namegenerators.mangled_fused_number");
 }
